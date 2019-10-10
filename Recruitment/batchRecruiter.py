44a9a40b7b2576b60955cbb0ea1ebd8272c2562f
Ian <47256454+IanRFerguson@users.noreply.github.com>
2019-10-10 17:47:04 -0400

    Update batchRecruiter.py

--- Recruitment/batchRecruiter.py
+++ Recruitment/batchRecruiter.py
@@ -9,13 +9,14 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import xlsxwriter
+import re
 
 now = datetime.datetime.now()
 
 port = 587
 smtp_server = 'smtp.gmail.com'
-my_address = input("Your email:\t\t\t")
-password = input("Enter your password:\t\t") # Not necessary if running locally, of course
+my_address = input("Your email:\t\t")
+password = input("Password:\t\t")
 cc = 'irf229@nyu.edu'
 
 emailSetup = smtplib.SMTP(host = smtp_server, port = port)
@@ -28,6 +29,8 @@ all_participants = pd.read_excel('participantList.xlsx')
 all_participants['Contacted'].fillna(0, inplace = True)
 all_participants['Date'].fillna('None Specified', inplace = True)
 all_participants['Time'].fillna('None Specified', inplace = True)
+all_participants['Responded'].fillna(' ', inplace = True)
+all_participants['Notes'].fillna(' ', inplace = True)
 
 for index, participant in enumerate(all_participants['Participant Name']):
 
@@ -37,26 +40,26 @@ for index, participant in enumerate(all_participants['Participant Name']):
 
         now = datetime.datetime.now()
 
-        contact_name = participant
+        contact_first_name = (re.findall('[A-Z][^A-Z]*', participant))[0]
         contact_email = all_participants['Participant Email'][index]
 
         htmlBody = ("""
         <html>
         <head> </head>
         <body>
-        <p> Hi """ + contact_name + """,
+        <p> Hi """ + contact_first_name + """,
         <br> <br>
         I hope this message finds you well! Our records show that you have previously participated in a research study with the Social Cognitive and Neural Sciences Lab at NYU, and I'm contacting you to see if you'd like to participate in another one.
         <br> <br>
-        We have several screening criteria for this study. You'll be eligible if you are:
+        Since this is a neuroimaging study using fMRI, we have several screening criteria to make sure that you're eligible to participate. Please confirm the following:
+        <br><br>
+        - You are right handed <br>
+        - You have Normal or Corrected-to-Normal Vision <br>
+        - You have no history of neurological disease <br>
+        - You're not currently taking psychoactive medication <br>
         <br>
-        - Right Handed <br>
-        - Normal or Corrected-to-Normal Vision <br>
-        - No history of Neurological Disease <br>
-        - Not currently taking psychoactive medication <br>
-        <br>
-        <p> This particular study will last about 90 minutes, and we can offer <b>$Y</b> in compensation.
-        If you think you might be interested, please feel free to <a href="mailto:irf229@nyu.edu"> contact me directly</a>. <br> <br>
+        <p> This particular study will last about 90 minutes, and we can offer <b>$60</b> in compensation.
+        If you think you might be interested, please feel free to <a href="mailto:irf229@nyu.edu"> contact me directly</a> to set up an appointment time. We'll be scanning exclusively on the weekends between 11 AM and 4 PM, if that works for your schedule.<br> <br>
         Thanks so much, I'll look forward to hearing from you! <br><br>
         <b>Ian Richard Ferguson</b><br>
         Research Assistant | Social Cognitive and Neural Sciences Lab <br>
@@ -78,7 +81,7 @@ for index, participant in enumerate(all_participants['Participant Name']):
         msg.attach(MIMEText(htmlBody, "html"))
         emailSetup.sendmail(from_addr= my_address, to_addrs = contact_email, msg = msg.as_string())
 
-        print("Contacting " + contact_name)
+        print("Contacting " + contact_first_name)
         all_participants.loc[index, 'Contacted'] = now.strftime('%x') + ' ' + now.strftime('%X')
 
         sleep(5)
@@ -93,7 +96,7 @@ for col in all_participants:
 
 manifest = xlsxwriter.Workbook('participantList.xlsx')
 main_sheet = manifest.add_worksheet('Participant Log')
-main_sheet.set_column('A:E', 28)
-main_sheet.add_table('A1:E31', {'data': all_participants.stack(), 'columns':header_list})
+main_sheet.set_column('A:G', 28)
+main_sheet.add_table('A1:G31', {'data': all_participants.stack(), 'columns':header_list})
 
 manifest.close()
