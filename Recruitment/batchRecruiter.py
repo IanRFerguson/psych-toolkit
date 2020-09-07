552e184f671f7ee61f5ec2c538a68b4516a0798e
Ian <47256454+IanRFerguson@users.noreply.github.com>
2020-09-06 21:36:10 -0700

    Update batchRecruiter.py

--- Recruitment/batchRecruiter.py
+++ Recruitment/batchRecruiter.py
@@ -1,5 +1,6 @@
+#!/usr/bin/env python
+# coding: utf-8
 # NOTE: This script requires an xlsx file named "participantList.xlsx" to run. I've included a template.
-
 # Script will find any participant entries that have been contacted and email them
 
 import pandas as pd
@@ -14,40 +15,41 @@ import re
 now = datetime.datetime.now()
 
 port = 587
-smtp_server = 'smtp.gmail.com'
-my_address = # INPUT EMAIL ADDRESS !
-password = # INPUT EMAIL PASSWORD !
-cc = # OPTIONAL
+smtp_server = 'smtp.gmail.com'                                              # Fill these in with your email + PW
+my_address = ''
+password = ''
+cc = ''
 
-emailSetup = smtplib.SMTP(host = smtp_server, port = port)
-emailSetup.ehlo()
-emailSetup.starttls()
-emailSetup.login(user = my_address, password = password)
 
-all_participants = pd.read_excel('participantList.xlsx')
+# -------------- HELPER FUNCTIONS
 
-all_participants['Contacted'].fillna(0, inplace = True)
-all_participants['Date'].fillna(' ', inplace = True)
-all_participants['Time'].fillna(' ', inplace = True)
-all_participants['Responded'].fillna(' ', inplace = True)
-all_participants['Notes'].fillna(' ', inplace = True)
+def participantData():
+    """
+    Reads local XLSX file into Pandas DataFrame
+    """
 
-for index, participant in enumerate(all_participants['Participant Name']):
+    all_participants = pd.read_excel('participantList.xlsx')
 
-    if all_participants['Contacted'][index] == 0:
+    # Replace null values with empty cells
+    all_participants['Contacted'].fillna(0, inplace = True)
+    all_participants['Date'].fillna(' ', inplace = True)
+    all_participants['Time'].fillna(' ', inplace = True)
+    all_participants['Responded'].fillna(' ', inplace = True)
+    all_participants['Notes'].fillna(' ', inplace = True)
 
-        # Participant has not yet been contacted
+    return all_participants
 
-        now = datetime.datetime.now()
+def formatMessage(NAME):
+    """
+    Formats HTML email body with participant info
+    Note - You'll want to change some of the text in here!
+    """
 
-        contact_first_name = (re.findall('[A-Z][^A-Z]*', participant))[0]
-        contact_email = all_participants['Participant Email'][index]
-
-        htmlBody = ("""
+    htmlBody = ("""
         <html>
         <head> </head>
         <body>
-        <p> Hi """ + contact_first_name + """,
+        <p> Hi """ + NAME + """,
         <br> <br>
         I hope this message finds you well! Our records show that you have previously participated in a research study with the Social Cognitive and Neural Sciences Lab at NYU, and I'm contacting you to see if you'd like to participate in another one.
         <br> <br>
@@ -67,53 +69,87 @@ for index, participant in enumerate(all_participants['Participant Name']):
         </body>
         </html>""")
 
-        emailSetup = smtplib.SMTP(host = smtp_server, port = port)
-        emailSetup.ehlo()
-        emailSetup.starttls()
-        emailSetup.login(user = my_address, password = password)
+    return htmlBody
 
-        msg = MIMEMultipart()
-        msg['From'] = my_address
-        msg['To'] = contact_email
-        msg['Cc'] = 'irf229@nyu.edu'
-        msg['Subject'] = 'fMRI Study Recruitment'
+def sendEmail(NAME, EMAIL):
+    """
+    Writes email to participant with text from formatMessage() function
+    """
 
-        msg.attach(MIMEText(htmlBody, "html"))
-        emailSetup.sendmail(from_addr= my_address, to_addrs = contact_email, msg = msg.as_string())
+    # I can't remember why I defined this three different times lol
+    now = datetime.datetime.now()
 
-        print("Contacting " + contact_first_name)
-        all_participants.loc[index, 'Contacted'] = str(now.strftime('%x')) + ' ' + str(now.strftime('%X'))
+    # Text for the email itself
+    messageText = formatMessage(NAME)
 
-        sleep(5)
+    # Format email itself (to/from/body/etc)
+    msg = MIMEMultipart()
+    msg['From'] = my_address
+    msg['To'] = EMAIL
+    msg['Cc'] = 'irf229@nyu.edu'
+    msg['Subject'] = 'fMRI Study Recruitment'
 
+    # Send that puppy!
+    msg.attach(MIMEText(messageText, "html"))
+    emailSetup.sendmail(from_addr= my_address, to_addrs = EMAIL, msg = msg.as_string())
+
+
+def cleanUp(IX):
+    """
+    Converts remaining columns to string format, for excel's sake
+    """
+
+    pData["Contacted"].loc[IX] = str(now.strftime('%x')) + ' ' + str(now.strftime('%X'))
+    pData["Date"].loc[IX] = str(" ")
+    pData["Time"].loc[IX] = str(" ")
+
+def toExcel():
+    """
+    Converts updated DataFrame back to XLSX format, saves it locally
+    """
+
+    header_list = []
+
+    for col in pData:
+        pattern = {'header':col}
+        header_list.append(pattern)
+
+    start = 'A1'
+    end = 'G' + '' + str(len(pData['Participant Name']) + 1)
+    tableSize = start + ':' + end
+
+    manifest = xlsxwriter.Workbook('participantList.xlsx')
+    main_sheet = manifest.add_worksheet('Participant Log')
+    main_sheet.set_column('A:G', 28)
+    main_sheet.add_table(tableSize, {'data': pData.stack(), 'columns':header_list})
+
+    manifest.close()
+
+# -------------- RIPPER
+pData = participantData()
+
+# Setup connection to gmail server with SMTPLib package
+emailSetup = smtplib.SMTP(host = smtp_server, port = port)
+emailSetup.ehlo()
+emailSetup.starttls()
+emailSetup.login(user = my_address, password = password)
+
+# Loop through participant rows
+for index, name in enumerate(pData["Participant Name"]):
+
+    if name == ' ':                                                     # Skip blank rows
+        continue
+
+    if pData["Contacted"][index] == 0:                                  # Don't contact a participant > 1 time
+        name = (re.findall('[A-Z][^A-Z]*', name))[0]                    # First name only
+        sendEmail(name, pData["Participant Email"][index])              # Send email
+        cleanUp(index)                                                  # Update other columns in participant row
+
+        print("Contacting {}...".format(str(name)))                     # Print message to confirm
+        sleep(5)                                                        # 5s lapse b/w participants
+
+print("\n\nAll participants contacted")
+
+# Push updated dataframe to Excel after all participants contacted
+toExcel()
 emailSetup.quit()
-
-for index, contacted in enumerate(all_participants['Contacted']):
-
-    all_participants['Contacted'].loc[index] = str(contacted)
-
-for index, date in enumerate(all_participants['Date']):
-
-    all_participants['Date'].loc[index] = str(date)
-
-for index, time in enumerate(all_participants['Time']):
-
-    all_participants['Time'].loc[index] = str(time)
-
-
-header_list = []
-
-for col in all_participants:
-    pattern = {'header':col}
-    header_list.append(pattern)
-
-start = 'A1'
-end = 'G' + '' + str(len(all_participants['Participant Name']) + 1)
-tableSize = start + ':' + end
-
-manifest = xlsxwriter.Workbook('participantList.xlsx')
-main_sheet = manifest.add_worksheet('Participant Log')
-main_sheet.set_column('A:G', 28)
-main_sheet.add_table(tableSize, {'data': all_participants.stack(), 'columns':header_list})
-
-manifest.close()
